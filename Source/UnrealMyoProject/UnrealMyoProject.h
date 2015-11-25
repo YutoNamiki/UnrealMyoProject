6cc77243f4d871114d1c1e78c2ac60eb35b6aeef
namiki <whiterunner.amanchu@gmail.com>
2015-11-25 19:23:44 +0900

    C++プロジェクトの作成

---
+++ Source/UnrealMyoProject/UnrealMyoProject.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __UNREALMYOPROJECT_H__
+#define __UNREALMYOPROJECT_H__
+
+#include "EngineMinimal.h"
+
+#endif
---
+++ Source/UnrealMyoProject/UnrealMyoProjectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "UnrealMyoProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealMyoProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealMyoProjectGameMode();
+};
+
+
+
